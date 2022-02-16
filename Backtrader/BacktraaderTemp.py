588b0daacb0c115fe9f883fa5c1e4eed9de0f523
Lenia <lenia_malki@hotmail.com>
2022-02-16 19:51:38 +0100

    Börjat med ett skal till backtrader

--- Backtrader/BacktraaderTemp.py
+++ Backtrader/BacktraaderTemp.py
@@ -1,7 +1,8 @@
 import os
-import backtrader as bt
-import backtrader.feeds as btfeed
 import sys  # To find out the script name (in argv[0])
+
+import backtrader as bt
+
 from Strategies import Strategy_1  # import our first strategy
 
 # Instantiate Cerebro engine. This is the main control center / brain
@@ -11,21 +12,22 @@ cerebro = bt.Cerebro()
 modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 # TODO: create generic data path reader
-datapath = os.path.join(modpath, '../../TradingBot/Data/AMZN.csv')
-
-# Create a Data Feed
-# TODO: create generic data path reader
-data = btfeed.YahooFinanceCSVData(dataname=datapath)
-
-# Add the Data Feed to Cerebro
-cerebro.adddata(data)
+tickers = ['AMZN', 'AAPL']
+for ticker in tickers:
+    datapath = os.path.join(modpath, '../../TradingBot/Data/{}.csv')
+    data = bt.feeds.YahooFinanceCSVData(
+        dataname=datapath.format(ticker))
+    cerebro.adddata(data)  # Add the Data Feed to Cerebro
 
 # Set starting value of portfolio
-cerebro.broker.setcash(1000000)
+cerebro.broker.setcash(100000.0)
 
 # Add strategy to Cerebro
 # TODO: allow for strategy switching
 cerebro.addstrategy(Strategy_1)
+
+# Set the commission - 0.1% ... divide by 100 to remove the %
+cerebro.broker.setcommission(commission=0.001)
 
 # Print starting portfolio value
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
@@ -35,3 +37,10 @@ cerebro.run()
 
 # Print final portfolio value
 print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+"""
+Requires matplotlib==3.2.2. Run the following in order to execute:
+pip uninstall matplotlib
+pip install matplotlib==3.2.2
+"""
+# cerebro.plot()
--- Backtrader/Strategies.py
+++ Backtrader/Strategies.py
@@ -15,8 +15,10 @@ class Strategy_1(bt.Strategy):
         # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
 
-        # Keep track of order status
+        # To keep track of pending orders and buy price/commission
         self.order = None
+        self.buyprice = None
+        self.buycomm = None
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -27,9 +29,19 @@ class Strategy_1(bt.Strategy):
         # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log('BUY EXECUTED, %.2f' % order.executed.price)
-            elif order.issell():
-                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
 
             self.bar_executed = len(self)
 
@@ -39,9 +51,17 @@ class Strategy_1(bt.Strategy):
         # Write down: no pending order
         self.order = None
 
-    # Where we define when to buy
+    # Receives a trade whenever there has been a change in one
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
+    # Defines when to buy and sell
     def next(self):
-        # Simply log the closing price of the series from the reference
+        # Log the closing price of the series from the reference
         self.log('Close, %.2f' % self.dataclose[0])
 
         # Check if an order is pending ... if yes, we cannot send a 2nd one
@@ -49,7 +69,7 @@ class Strategy_1(bt.Strategy):
             return
 
         # Check if we are in the market
-        if not self.position:
+        if not self.position:  # Returns the current position for a given data in a given broker.
 
             # Not yet ... we MIGHT BUY if ...
             if self.dataclose[0] < self.dataclose[-1]:

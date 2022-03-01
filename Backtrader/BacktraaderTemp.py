5570619c98419c1cd4ced547501fb8ed37c9d887
Lenia <lenia_malki@hotmail.com>
2022-03-01 13:33:00 +0100

    CSV reader

--- Backtrader/BacktraaderTemp.py
+++ Backtrader/BacktraaderTemp.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 pd.options.mode.chained_assignment = None
 
-from Strategies import *  # import our first strategy
+from Strategies import *
 
 # Instantiate Cerebro engine. This is the main control center / brain
 cerebro = bt.Cerebro()
@@ -18,7 +18,8 @@ modpath = os.path.dirname(os.path.dirname(sys.argv[0]))
 # tickers = ['AAPL', 'AMZN','NFLX']
 # tickers = ['NFLX', 'AMZN']
 # tickers = ['AMZN_Nas', 'AAPL_Nas']
-tickers = ['AMZN (1)', 'NFLX']
+# tickers = ['AMZN (1)', 'NFLX']
+tickers = ['AAPL_1min', 'AMZN_1min']
 
 '''
 for ticker in tickers:
@@ -30,92 +31,55 @@ for ticker in tickers:
 
 
 # Used for reformatting data from Nasdaq. Run only once!
-def reformatData(pathToFile):
-    df = pd.read_csv(pathToFile)
+def reformatData(pathToFile, outputName):
+    cs = os.path.join(modpath, 'Data/{}')  # Holder
 
-    for i in range(len(df.index)):
-        x = df['Date'][i]
-        reformated_date = datetime.datetime.strptime(x, '%m/%d/%Y').strftime("%Y-%m-%d")
-        df['Date'][i] = reformated_date
+    df = pd.read_csv(pathToFile, sep=',', header=None)
 
-        x = df['Close/Last'][i]
-        y = x.replace('$', '')
-        df['Close/Last'][i] = y
+    header = ["DateTime", "Open", "High", "Low", "Close", "Volume"]
+    df.columns = header
 
-        x = df['Open'][i]
-        y = x.replace('$', '')
-        df['Open'][i] = y
+    new = cs.format(outputName)
 
-        x = df['High'][i]
-        y = x.replace('$', '')
-        df['High'][i] = y
-
-        x = df['Low'][i]
-        y = x.replace('$', '')
-        df['Low'][i] = y
-
-    df = df.iloc[::-1]  # Reverses order of dataframe
-    df.to_csv(pathToFile, index=False, )
+    df.to_csv(new, index=False, )
 
 
 for ticker in tickers:
-    datapath = os.path.join(modpath, 'Data/{}.csv')
-    file = datapath.format(ticker)
+    datapath = os.path.join(modpath, 'Data/{}.txt')  # Holder
+    file = datapath.format(ticker)  # Path to file, example Data/AAPL.txt
 
-    '''
-    # Uncomment if you want to reformat the data. Should not be run more than once. 
-    # reformatData(file)
+    csvPath = os.path.join(modpath, 'Data/{}.csv')  # Holder
+    csvFile = csvPath.format(ticker)  # Path to file, example Data/AAPL.csv
 
-    GenericCSVData() is used to parse different CSV formats. Below is for Nasdaq
+    i = '{}.csv'  # Holder
+    fileName = i.format(ticker)  # Example, AAPL.csv
+
+    # Converts txt files to CSV-files with added header
+    reformatData(file, fileName)
+
     data = bt.feeds.GenericCSVData(
-        dataname=file,
-        fromdate=datetime.datetime(2017, 2, 28),  # Ending  date
-        todate=datetime.datetime(2022, 2, 25),  # Starting date
+
+        dataname=csvFile,
+        fromdate=datetime.datetime(2021, 8, 2, 4, 00, 00),  # Ending  date
+        todate=datetime.datetime(2021, 2, 13, 19, 59, 00),  # Starting date
 
         nullvalue=0.0,  # Used for replacing NaN-values with 0
 
-        dtformat='%Y-%m-%d',  # used to parse the datetime CSV field. Default %Y-%m-%d
-        tmformat='%H.%M.%S',  # used to parse the time CSV field if “present”
+        dtformat='%Y-%m-%d %H:%M:%S',  # used to parse the datetime CSV field. Default %Y-%m-%d
+        tmformat='%H:%M:%S',  # used to parse the time CSV field if present
 
         datetime=0,  # column containing the date
         time=-1,  # column containing the time field if separate from the datetime field. -1 if not present.
 
         # For each below, reference the corresponding index from the data
-        high=4,
-        low=5,
-        open=3,
-        close=1,
-        volume=2,
-        openinterest=-1,  # -1 if no such column exists
-    )
-    
-    # df = pd.read_csv(file)
-    # df = df.iloc[::-1]  # Reverses order of dataframe
-    # df.to_csv(file, index=False, )  # Overwrite csv file
-    '''
 
-    # Data feed for Yahoo
-    data = bt.feeds.GenericCSVData(
-
-        dataname=file,
-        fromdate=datetime.datetime(2017, 2, 28),  # Ending  date
-        todate=datetime.datetime(2022, 2, 25),  # Starting date
-
-        nullvalue=0.0,  # Used for replacing NaN-values with 0
-
-        dtformat='%Y-%m-%d',  # used to parse the datetime CSV field. Default %Y-%m-%d
-        tmformat='%H.%M.%S',  # used to parse the time CSV field if “present”
-
-        datetime=0,  # column containing the date
-        time=-1,  # column containing the time field if separate from the datetime field. -1 if not present.
-
-        # For each below, reference the corresponding index from the data
+        open=1,
         high=2,
         low=3,
-        open=1,
         close=4,
-        volume=6,
-        openinterest=-1,  # -1 if no such column exists
+        volume=5,
+
+        openinterest=-1  # -1 if no such column exists
     )
     cerebro.adddata(data)
 
@@ -147,4 +111,4 @@ Requires matplotlib==3.2.2. Run the following in order to execute:
 pip uninstall matplotlib
 pip install matplotlib==3.2.2
 """
-cerebro.plot()
+# cerebro.plot()

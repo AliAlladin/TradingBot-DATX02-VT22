8c274311c273ab354b80e5dee0638e53559edc46
Ali Alladin <ali.alladin20@gmail.com>
2022-03-04 22:59:32 +0100

    Added function to get intraday data.

--- Alpaca/Alpaca.py
+++ Alpaca/Alpaca.py
@@ -1,14 +1,15 @@
-import alpaca_trade_api as tradeapi
+from alpaca_trade_api import REST, TimeFrame
 from Config import *
 import time
+from datetime import date, datetime, timedelta
 
 
 class Alpaca:
     def __init__(self):
-        self.api = tradeapi.REST(
+        self.api = REST(
             ALPACA_API_KEY, ALPACA_SECRET_KEY, BASE_URL, api_version='v2')
 
-    def getBalance(self):
+    def get_balance(self):
         return float(self.api.get_account().cash)
 
     # Function that checks if market is open.
@@ -23,6 +24,22 @@ class Alpaca:
             time_to_open = (clock.next_open - clock.timestamp).total_seconds()
             time.sleep(round(time_to_open))
 
+    def get_intraday_data(self, symbol):
+        current_time = self.api.get_clock().timestamp
+        start_time = current_time.replace(hour=9, minute=30).isoformat()
+        print(start_time)
+        fifteen_minutes = timedelta(minutes=15)
+        print(current_time)
+        end_time = current_time.replace(hour=16, minute=00).isoformat()
+        if self.market_is_open():
+            end_time = (current_time - fifteen_minutes).isoformat()
+
+        data = self.api.get_bars(symbol=symbol, start=start_time,
+                                 end=end_time, timeframe=TimeFrame.Minute)
+
+        return data.df
+
 
 broker = Alpaca()
-print(broker.api.get_account())
+
+print(broker.get_intraday_data("AAPL"))

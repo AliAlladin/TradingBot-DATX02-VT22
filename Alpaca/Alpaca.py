548a073d54fd3aca76c4f03d647635d8811797dd
Ali Alladin <ali.alladin20@gmail.com>
2022-03-04 22:09:35 +0100

    Added functions to check time.

--- Alpaca/Alpaca.py
+++ Alpaca/Alpaca.py
@@ -1,5 +1,6 @@
 import alpaca_trade_api as tradeapi
 from Config import *
+import time
 
 
 class Alpaca:
@@ -9,3 +10,19 @@ class Alpaca:
 
     def getBalance(self):
         return float(self.api.get_account().cash)
+
+    # Function that checks if market is open.
+    def market_is_open(self):
+        clock = self.api.get_clock()
+        return clock.is_open and (clock.next_close - clock.timestamp).total_seconds() > 120
+
+    # Function that puts the program to sleep until the market opens.
+    def wait_for_market_open(self):
+        clock = self.api.get_clock()
+        if not clock.is_open:
+            time_to_open = (clock.next_open - clock.timestamp).total_seconds()
+            time.sleep(round(time_to_open))
+
+
+broker = Alpaca()
+print(broker.api.get_account())

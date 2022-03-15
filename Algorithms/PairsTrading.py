ac72230ec82d12f9473379d75a986e9a6ccde34a
Lenia <lenia_malki@hotmail.com>
2022-03-15 17:37:32 +0100

    Updated the strategy so it does not depend on alpaca

--- Algorithms/PairsTrading.py
+++ Algorithms/PairsTrading.py
@@ -1,122 +1,79 @@
 import os
 import sys  # To find out the script name (in argv[0])
-from MainSystem import Main
-import matplotlib.pyplot as plt
+
 import numpy as np
 import statsmodels.api as sm
-import alpaca_trade_api as tradeapi
-import pandas as pd
-from alpaca_trade_api.rest import TimeFrame
 from IPython.display import display
+import pandas as pd
 
-pd.options.display.max_columns = None
+'''
+This class receives a dataframe containing the latest prices for each ticker.
+It will go through each ticker, find its pair and perform the strategy on that pair in order
+to check whether or not we should sell or buy a either of the stocks of that pair. In that case,
+a sell/buy sigal will be passed back to Main. 
+'''
+# ----------------------------------------------------------------------------------------------------------------------
+# These dataframes should be provided by Main, in the same format as below
+example_input = {'Symbol': ['AMZN', 'AA', 'AAPL', 'A'], 'Price': [2837.06, 73.50, 150.62, 127.58]}
+df = pd.DataFrame(example_input)
+
+# Queried historical daily bar data
+hist_data = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(sys.argv[0])), 'Algorithms/hist_data.csv'))
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Converting Pairs.txt to a dataframe
+pairs = pd.read_csv(os.path.join(os.path.dirname(os.path.dirname(sys.argv[0])), 'Backtrader/Pairs.txt'), sep=" ",
+                    header=None)
+pairs.columns = ['T1', 'T2']
 
 # The parameters that are to be varied to optimize the model
 distance = 3
-period = 25
+period = 500
 invested_amount = 10000
+trade_taken = False
 
-# Individual os paths
-modpath = os.path.dirname(os.path.dirname(sys.argv[0]))
-datap = os.path.join(modpath, 'Backtrader/Pairs.txt')
+# Iterate through all pairs
+for i in range(len(pairs.index)):
+    # Fetches pair and their corresponding, updated prices from the dataframe provided by Main
+    t1 = pairs['T1'][i]  # Ticker symbol
+    t2 = pairs['T2'][i]  # Ticker symbol
 
-my_pair_df = pd.read_csv(datap, sep=" ")
+    tick1 = df.loc[df['Symbol'] == t1]  # Minute data for ticker
+    tick2 = df.loc[df['Symbol'] == t2]  # Minute data for ticker
 
-# Connection
-base_url = 'https://paper-api.alpaca.markets'
-ALPACA_API_KEY = 'PKVIY9JARA7VQ6WC5M50'
-data_url = 'wss://data.alpaca.markets'
-ALPACA_SECRET_KEY = '5Y2d1GUGUhr8NWilj5vTZ1oB71ybC8eoRN6czMIh'
-api = tradeapi.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, base_url)
-conn = tradeapi.stream.Stream(ALPACA_API_KEY, ALPACA_SECRET_KEY, base_url)
-isActive = False
-long_first_stock = False
+    '''
+    Creates a new dataframe with only the historical closing prices for the specific pair.
+    By default, the Alpaca API will return today's candle even though it is not closed. This is incomplete.
+    We update the last row later.
+    '''
+    data_df = pd.concat(
+        [hist_data[t1], hist_data[t2]],
+        axis=1,
+        join='inner',
+        keys=[t1, t2],
+    )
 
-t1 = my_pair_df.columns[0]  # Name of ticker
-t2 = my_pair_df.columns[1]  # Name of ticker
-t1_frame = api.get_bars(t1, TimeFrame.Day, "2020-09-30", "2020-11-03", adjustment='raw').df
-t2_frame = api.get_bars(t2, TimeFrame.Day, "2020-09-30", "2020-11-03", adjustment='raw').df
-data_df = pd.concat(
-    [t1_frame.close, t2_frame.close],
-    axis=1,
-    join='inner',
-    keys=[t1, t2],
-)
-# path = os.path.join(modpath, 'Algorithms/Hello.csv')
-# ____.to_csv(path,index=False)
+    # Update the last row with the minute data
+    data_df.iloc[-1] = [tick1.iat[0, 1], tick2.iat[0, 1]]
 
-# Perform a linear regression to calculate the spread
-result = sm.OLS(data_df[t1], data_df[t2]).fit()
-beta = result.params[0]
-spread = []
-for i in range(0, period):
-    spread.append(data_df[t1][i] - beta * data_df[t2][i])
-spread_df = data_df.copy()
-spread_df['spread'] = spread
+    # Perform a linear regression to calculate the spread
+    result = sm.OLS(data_df[t1], data_df[t2]).fit()
+    beta = result.params[0]
+    spread = []
 
-mean = spread_df['spread'].mean()
-std = spread_df['spread'].std()
-z_score = (spread[period - 1] - mean) / std
-print('Z: ' + str(z_score))
-# To know how much we need to buy of each stock
-shares_stock1 = invested_amount / data_df[t1][period - 1]
-current_ratio = data_df[t1][period - 1] / data_df[t2][period - 1]
+    for k in range(0, len(data_df)):
+        spread.append(data_df[t1][k] - beta * data_df[t2][k])
 
-# If we don't have a position in this pair
-if not isActive:
-    # We check whether the Z-score is unusually high or low (>distance or <-distance)
-    if z_score > distance:
-        # High Z-score, we sell stock 1 and buy stock 2
+    # Calculation of the Z-score
+    mean = np.mean(spread)
+    std = np.std(spread)
+    z_score = (spread[len(data_df) - 1] - mean) / std
 
-        long_first_stock = False  # true when we are long of the first stock
-        ratio = current_ratio
-        shares_stock1 = shares_stock1
-        trade_taken = True
+    # To know how much we need to buy of each stock
+    shares_stock1 = invested_amount / data_df[t1][len(data_df) - 1]
+    current_ratio = data_df[t1][len(data_df) - 1] / data_df[t2][len(data_df) - 1]
 
-        print('Strategy sent out sell signal')
-        Main.buy_sell_signal = 'Sell'
+    print(mean, std, z_score)
 
-    # The Z-score is unusually low, we buy stock1 and sell stock2
-    elif z_score < distance:
-
-        # Description of our position
-        long_first_stock = True
-        shares_stock1 = shares_stock1
-        ratio = current_ratio
-        isActive = True
-
-        # buy stock1 and sell stock2
-        print('Strategy sent out signal')
-
-        signal = [{'B', t1}, {'S', t2}]
-        Main.setSignal(signal)
-
-# We have a position on a pair and therefore examine whether to close it
-else:
-    # We previously bought stock 1
-    if long_first_stock:
-        if z_score > 0:
-            # Sell stock 1 and buy back stock 2
-            signal = [{'S', t1}, {'B', t2}]
-            Main.setSignal(signal)
-
-            # We close the position in the pair
-            isActive = False
-            shares_stock1 = None
-            ratio = None
-    else:
-
-        if z_score < 0:
-            # Buy back stock 1 and sell stock 2
-            signal = [{'B', t1}, {'S', t2}]
-            Main.setSignal(signal)
-
-            '''
-            # Calculating the profit of the pairs trading
-            profit = pair.shares_stock1 * pair.ratio * self.datas[self.dic.get(pair.stock2)] \
-                     - pair.shares_stock1 * self.datas[self.dic.get(pair.stock1)]
-            '''
-            # We close the position in the pair
-            isActive = False
-            shares_stock1 = None
-            ratio = None
+    # while not trade_taken:
+    # print('Hello')

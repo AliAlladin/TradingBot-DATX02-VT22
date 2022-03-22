62f4aeb7994511e00fbcb800e1a6682d154b665c
Isak15 <nilisak@student.chalmers.se>
2022-03-22 18:18:31 +0100

    h

--- Backtrader/FindPairs.py
+++ Backtrader/FindPairs.py
@@ -13,78 +13,122 @@ import os
 import sys  # To find out the script name (in argv[0])
 from datetime import timedelta, date
 import math
+import random
 
-def main():   
 
-    start = '2012-02-03'
-    end = '2015-02-03'
+def main():
+    start = '2011-01-01'
+    end = '2014-01-01'
+    #distinctStocks()
+    # tryingOut(start,end)
     stocks=acquireList()
-    #stocks=stocks[0:30]
+    stocks=stocks[0:30]
     pairs=findPairs(stocks,start,end)
     my_pair_file = open('Pairs.txt', 'w')
     for pair in pairs:
         stock1, stock2=pair.get_pairs()
         my_pair_file.write(stock1+" "+stock2+ "\n")
     my_pair_file.close()
+    pairs=acquiringPair(pairs)
+    my_pair_file = open('Pairs1.txt', 'w')
+    for pair in pairs:
+        stock1, stock2 = pair.get_pairs()
+        my_pair_file.write(stock1 + " " + stock2 + "\n")
+    my_pair_file.close()
 
 
 def acquireList():
-    stocks=[]
+    stocks = []
     modpath = os.path.dirname(os.path.dirname(sys.argv[0]))
     directory_in_str = os.path.join(modpath, 'Data/filtered_csv_data/')
     directory = os.fsencode(directory_in_str)
     for filename in os.listdir(directory):
-        x=(str(filename))
-        x=x.split('\'')[1]
-        x=x.removesuffix('.csv')
+        x = (str(filename))
+        x = x.split('\'')[1]
+        x = x.removesuffix('.csv')
         if "." in x:
-            y=x.split('.')
-            x=y[0]+"-"+y[1]
+            y = x.split('.')
+            x = y[0] + "-" + y[1]
         stocks.append(x)
     return stocks
 
-def findPairs(stocks,start,end):
+
+def tryingOut(start, end):
+    stock = 'AIG'
+    prices = yf.download(stock, start, end)
+    print(prices)
+
+
+def distinctStocks():
+    my_pair_file = open('Backtrader/Pairs.txt', 'r')
+    distinctStocks = []
+    for i in my_pair_file:
+        for j in i.split():
+            if j not in distinctStocks:
+                distinctStocks.append(j)
+    print(len(distinctStocks))
+    print(distinctStocks)
+
+
+def acquiringPair(pairs):
+    distinctStocks = []
+    random.shuffle(pairs)
+    newPairs = []
+    for pair in pairs:
+        add = True
+        stocks = pair.getpairs()
+        for stock in stocks:
+            if stock in distinctStocks:
+                add = False
+        if add == True:
+            distinctStocks.append(stock[0])
+            distinctStocks.append(stock[1])
+            newPairs.append(pair)
+
+
+def findPairs(stocks, start, end):
     window = 252
     data = pd.DataFrame()
     pairs = []
-    stocksNew=[]
+    stocksNew = []
     tic = time.perf_counter()
-    for i in range(0,len(stocks)):
-        prices = yf.download(stocks[i],start,end)
+    for i in range(0, len(stocks)):
+        prices = yf.download(stocks[i], start, end)
         if not prices.empty:
-            firstDayForStock=prices.index[1]
-            firstDayForStock=str(firstDayForStock).split()[0]
-            lastDayForStock=prices.index[-1]
+            firstDayForStock = prices.index[1]
+            firstDayForStock = str(firstDayForStock).split()[0]
+            lastDayForStock = prices.index[-1]
             lastDayForStock += timedelta(days=1)
-            lastDayForStock=str(lastDayForStock).split()[0]
-            
-            if str(firstDayForStock)==start and lastDayForStock==end:
-                data[stocks[i]]=(prices['Close'])
+            lastDayForStock = str(lastDayForStock).split()[0]
+
+            if str(firstDayForStock) == start and lastDayForStock == end:
+                data[stocks[i]] = (prices['Close'])
                 stocksNew.append(stocks[i])
     toc = time.perf_counter()
     # print('downloading data took ' , toc-tic , 'seconds')
-    stocks=stocksNew
+    stocks = stocksNew
     print(len(stocks))
     tic = time.perf_counter()
-    for i in range(0,len(stocks)):
+    for i in range(0, len(stocks)):
         print(i)
-        for j in range(i+1, len(stocks)):
+        for j in range(i + 1, len(stocks)):
             stock1data = data[stocks[i]].tolist()
             stock2data = data[stocks[j]].tolist()
-            stock1data=np.log10(stock1data)
-            stock2data=np.log10(stock2data)
-            result = sm.OLS(stock1data,sm.add_constant(stock2data)).fit()
-            alpha=result.params[0]
+            stock1data = np.log10(stock1data)
+            stock2data = np.log10(stock2data)
+            result = sm.OLS(stock1data, sm.add_constant(stock2data)).fit()
+            alpha = result.params[0]
             beta = result.params[1]
-            p1 = adfuller(stock1data-beta*stock2data)[1]
+            p1 = adfuller(stock1data - beta * stock2data)[1]
             p2 = coint(stock1data, stock2data)[1]
-
-            if p1 <0.0002 and p2<0.0002:
-                p = Pair(stocks[i],stocks[j])
+            if p1 < 0.01 and p2 < 0.01:
+                p = Pair(stocks[i], stocks[j])
                 pairs.append(p)
     toc = time.perf_counter()
     plt.show()
-    print('finding pairs took ' , toc - tic , ' seconds')
+    print('finding pairs took ', toc - tic, ' seconds')
     print(len(pairs))
     return pairs
+
+
 main()

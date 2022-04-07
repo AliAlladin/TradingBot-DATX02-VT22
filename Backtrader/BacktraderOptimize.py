9aedaba5d1b1cd29dc5867d140237317ff588b6c
OskarHP1 <paoskar@student.chalmers.se>
2022-04-07 17:07:49 +0200

    Renskriven version av BacktraderOptimize

--- Backtrader/BacktraderOptimize.py
+++ Backtrader/BacktraderOptimize.py
@@ -1,45 +1,40 @@
-from optimizeStrats import *  # import our first strategy.
-import os
-import sys  # To find out the script name (in argv[0])
-import backtrader.analyzers as btanalyzers
+import os  # To find out the script name (in argv[0])
+import sys
 import time
+import backtrader.analyzers as btanalyzers
 import pandas as pd
+from optimizeStrats import *  # import our first strategy.
 
-# TODO: What is this?
-pd.options.mode.chained_assignment = None
+# Global variables
+modpath = os.path.dirname(os.path.dirname(sys.argv[0]))  # Individual os paths
+start_date = datetime.datetime(2013, 9, 5, 9, 30, 00)
+end_date = datetime.datetime(2019, 1, 7, 16, 00, 00)
 
 
 # To optimize the strategy
 def optimize(strategy):
-
     tic = time.perf_counter()
 
     cerebro = bt.Cerebro()  # Instantiate Cerebro engine. This is the main control center/brain
     starting_budget = 100000.0  # The amount of money we start with in the optimization
-    # Set starting value of portfolio
-    cerebro.broker.setcash(starting_budget)
+    cerebro.broker.setcash(starting_budget)  # Set starting value of portfolio
+    cerebro.broker.setcommission(commission=0)  # We do not have any commission
 
-    modpath = os.path.dirname(os.path.dirname(sys.argv[0]))  # Individual os paths
-
-    # We run optimize for the Pair trading strategy
+    # To get which tickers we should optimize for
     if strategy == 'Pair':
         tickers = get_tickers_from_pairs(modpath)  # We add the tickers (ONCE!) from all pairs
-
-    # We run optimize for the Fibonacci retracement strategy
     else:
-        tickers = get_tickers_from_stocks(modpath)  # We add all the tickers
+        tickers = get_tickers_from_stocks(modpath)
 
-    # We add the data for all tickers
-    add_data(cerebro, modpath, tickers)
-    cerebro.broker.setcommission(commission=0)  # We do not have any commission
+    add_data(cerebro, tickers)  # We add the data for all tickers
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())  # Starting portfolio value
 
-    # To declare which strategy we choose to optimize
+    # To define the parameters we optimize for
     if strategy == 'Pair':
         # The values of what we want to run the strategy for
-        last_date = datetime.date(2019, 1, 5)   # Last date to know when to close positions
-        distance = np.linspace(2, 3, num=5)     # Decides when to buy or sell
-        period = np.linspace(450, 650, num=4)   # For how long time in the path we look for deviations
+        last_date = end_date.date()  # Last date to know when to close positions
+        distance = np.linspace(0.5, 1, num=2)   # Decides when to buy or sell
+        period = range(400, 500, 50)            # For how long time in the path we look for deviations
         maximum = max(period)                   # The maximum of period, to make sure all runs starts at the same date
         invested_amount = 10000                 # The amount we invest in each pair
 
@@ -51,17 +46,18 @@ def optimize(strategy):
                                      invested=invested_amount)
 
     else:
-        period = range(10, 100, 15)  # The period for which we look for highs and lows
-        maximum = max(period)        # The maximum of period
+        period = np.linspace(10, 85, num=6)     # The period for which we look for highs and lows
+        maximum = max(period)                   # The maximum of period
+        invested_amount = 1000
 
         strats = cerebro.optstrategy(Strategy_fibonacci2,
-                                     invested=1000,
+                                     invested=invested_amount,
                                      period=period,
                                      maximum=maximum)
 
     run_optimize(cerebro)
     toc = time.perf_counter()
-    print('running optimize took ', toc-tic, 'seconds')
+    print('Running optimize took ', toc-tic, 'seconds.')
 
 
 def get_tickers_from_pairs(path):
@@ -89,7 +85,7 @@ def get_tickers_from_pairs(path):
 
 def get_tickers_from_stocks(path):
     # The data of pairs comes from Pairs.txt which we read
-    data_path = os.path.join(path, 'Backtrader/Pairs.txt')
+    data_path = os.path.join(path, 'Backtrader/Stocks.txt')
     my_stock_file = open(data_path, 'r')
     tickers = []
     for stock in my_stock_file:
@@ -100,17 +96,14 @@ def get_tickers_from_stocks(path):
 
 
 # Adds the data for ticker in tickers in path modpath to cerebro
-def add_data(cerebro, modpath, tickers):
+def add_data(cerebro, tickers):
     # We add the data to cerebro
     for ticker in tickers:
-        csv_file_path = os.path.join(modpath, 'Data/filtered_csv_data/{}.csv').format(
-            ticker)  # Full path to csv-file
-
+        csv_file_path = os.path.join(modpath, 'Data/filtered_csv_data/{}.csv').format(ticker)  # Full path to csv-file
         data = bt.feeds.GenericCSVData(
-
             dataname=csv_file_path,  # Full path to csv-file
-            fromdate=datetime.datetime(2013, 9, 5, 9, 30, 00),  # Start  date
-            todate=datetime.datetime(2016, 1, 7, 16, 00, 00),  # Ending date
+            fromdate=start_date,  # Start  date
+            todate=end_date,  # Ending date
 
             nullvalue=0.0,  # Used for replacing NaN-values with 0
 
@@ -130,30 +123,20 @@ def add_data(cerebro, modpath, tickers):
 
             openinterest=-1,  # -1 if no such column exists
             timeframe=bt.TimeFrame.Minutes,
-            # compression=1
-
         )
         cerebro.adddata(data)
 
 
-# Runs the optimization through cerebro
+# # Core method to perform backtesting
 def run_optimize(cerebro):
 
-    # TODO: Remove this?
-    # Creates csv files with inquired data. Has to be executed before cerebro.run()
-    # "out" specifies the name of the output file. It currently overwrites the same file.
-    # cerebro.addwriter(bt.WriterFile, csv=True, out='log.csv')
-
-    # Core method to perform backtesting
-
-    # Print final portfolio value
     cerebro.addsizer(bt.sizers.PercentSizer, percents=10)
     cerebro.addanalyzer(btanalyzers.SharpeRatio, _name="sharpe")
     cerebro.addanalyzer(btanalyzers.DrawDown, _name="drawdown")
     cerebro.addanalyzer(btanalyzers.Returns, _name="returns")
 
     back = cerebro.run(maxcpus=1)
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())  # Print final portfolio value
 
     par_list = [[x[0].params.distance,
                  x[0].params.period,
@@ -162,7 +145,7 @@ def run_optimize(cerebro):
                  x[0].analyzers.sharpe.get_analysis()['sharperatio']] for x in back]
 
     par_df = pd.DataFrame(par_list, columns=['distance', 'period', 'return', 'dd', 'sharpe'])
-    # qgrid.show_grid(par_df).
+    print(par_df)
 
 
 optimize('Pair')

90cf6264c3e3e782c97c95917a74af4934b091f0
Ali Alladin <ali.alladin20@gmail.com>
2022-05-03 16:43:43 +0200

    Commented AlpacaBroker.py

--- Alpaca/AlpacaBroker.py
+++ Alpaca/AlpacaBroker.py
@@ -1,22 +1,27 @@
 import time
-
 import alpaca_trade_api as tradeapi
 import pandas as pd
-
 from Alpaca.Config import *
 
-
+'''
+Wrapper class for the alpaca_trade_api library. 
+'''
 class AlpacaBroker:
     def __init__(self):
         self.api = tradeapi.REST(
             ALPACA_API_KEY, ALPACA_SECRET_KEY, BASE_URL, api_version='v2')
         self.error = tradeapi.rest.APIError
 
-    # Function that checks if market is open.
+    '''
+    Function that checks if market is open.
+    '''
     def market_is_open(self):
         clock = self.api.get_clock()
         return clock.is_open and (clock.next_close - clock.timestamp).total_seconds() > 120
 
+    '''
+    Puts the programme to sleep if the market is not open.
+    '''
     def wait_for_market_open(self):
         clock = self.api.get_clock()
         if not clock.is_open:
@@ -24,7 +29,9 @@ class AlpacaBroker:
             time_to_open = (clock.next_open - clock.timestamp).total_seconds()
             time.sleep(round(time_to_open))
 
-    # Function to place a buy order.
+    '''
+    Function to place a buy order.
+    '''
     def buy(self, symbol: str, target_position_size: float):
         try:
             order = self.api.submit_order(
@@ -33,8 +40,9 @@ class AlpacaBroker:
             return order.id
         except Exception as e:
             print(e)
-
-    # Function to place a sell order.
+    '''
+    Function to place a sell order.
+    '''
     def sell(self, symbol: str, target_position_size: float):
         try:
             order = self.api.submit_order(symbol, target_position_size, "sell", "market", "day")
@@ -42,6 +50,9 @@ class AlpacaBroker:
         except Exception as e:
             print(e)
 
+    '''
+    Returns order information given order id.
+    '''
     def get_order(self, id: str):
         order = self.api.get_order(id)
 
@@ -55,6 +66,9 @@ class AlpacaBroker:
 
         return order_info
 
+    '''
+    Returns a list of all open orders.
+    '''
     def all_orders(self):
         orders = self.api.list_orders(limit=500)
         list = []
@@ -69,6 +83,9 @@ class AlpacaBroker:
             list.append(order_info)
         return pd.DataFrame(list)
 
+    '''
+    Returns position in a stock given ticker symbol.
+    '''
     def get_position(self, symbol: str):
         try:
             position = self.api.get_position(symbol=symbol)
@@ -86,6 +103,9 @@ class AlpacaBroker:
             print(e)
             return {}
 
+    '''
+    Returns a list of all current positions in the stocks. 
+    '''
     def all_postions(self):
         positions = self.api.list_positions()
         list = []
@@ -102,12 +122,21 @@ class AlpacaBroker:
             list.append(position_info)
         return pd.DataFrame(list)
 
+    '''
+    Returns account balance.
+    '''
     def get_balance(self):
         return float(self.api.get_account().cash)
 
+    '''
+    Returns portfolio value.
+    '''
     def get_portfolio_value(self):
         return float(self.api.get_account().equity)
 
+    '''
+    Filters out non-shortable stocks given a dataframe of pairs.
+    '''
     def get_shortable(self, pairs: pd.DataFrame):
         for index, row in pairs.iterrows():
             try:
